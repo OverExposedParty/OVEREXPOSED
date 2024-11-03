102ed0c4949b4accca21f5ce8caf6271a40c6c98
AlvinAhwieh <overexposedgame@gmail.com>
2024-11-03 21:31:12 +0000

    wheel update
    Wheel update

--- public/social-media-links.js
+++ public/social-media-links.js
@@ -1,10 +1,10 @@
 // Declare the variable with the desired URL
 const instagramUrl = "https://www.instagram.com/oe.app/";
-const tiktokUrl = "https://www.instagram.com/oe.app/";
+const tiktokUrl = "https://www.tiktok.com/@overexposed.app";
 
 // Select the anchor element
 const instagramLink = document.getElementById("instagram-link");
-const tiktokLink = document.getElementById("instagram-link");
+const tiktokLink = document.getElementById("tiktok-link");
 
 // Set the href attribute to the variable
 instagramLink.href = instagramUrl;
