18428cc37f8a334970fd674b35444911bc057cf7
AlvinAhwieh <overexposedgame@gmail.com>
2025-04-26 22:27:15 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
 
-# Make sure pocketbase is executable
-chmod +x ./pocketbase
 
 # Start node server.js in the background
-nohup node server.js &
-
-# Start pocketbase
-./pocketbase serve --http 143.110.162.204:8090
-
+nohup node server.js 
