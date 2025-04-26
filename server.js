b6ffdb4bf75e29e9cf04cc69047d6dea6d855aaf
AlvinAhwieh <overexposedgame@gmail.com>
2025-04-26 23:45:34 +0100

    update

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ const PORT = process.env.PORT || 3000;
 
 const { spawn } = require('child_process');
 
-// Start PocketBase server
-const pocketbasePath = path.join(__dirname, 'pocketbase.exe'); // Use 'pocketbase' on Mac/Linux
-const pb = spawn(pocketbasePath, ['serve'], {
-  cwd: __dirname,
-  stdio: 'inherit',
-});
-
-pb.on('error', (err) => {
-  console.error('Failed to start PocketBase:', err);
-});
 
 
 // Add security headers using helmet
--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 
 chmod +x ./pocketbase
 
-
-nohup ./pocketbase serve --http 143.110.162.204:8090
+nohup node server.js 
