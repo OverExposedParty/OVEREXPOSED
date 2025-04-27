34a8e2ae90e4e862d4d706c11ec7968d2294fe91
AlvinAhwieh <overexposedgame@gmail.com>
2025-04-27 02:09:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Give execute permission to pocketbase
-chmod +x ./pocketbase
+chmod +x /workspace/pocketbase
 
 # Run node server.js in the background and redirect output to a log file
 nohup node server.js > server.log 2>&1 &
