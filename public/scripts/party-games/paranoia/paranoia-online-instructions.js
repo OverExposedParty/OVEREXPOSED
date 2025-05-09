7431db25bb76925bc487e3b9846358fb542e87f3
AlvinAhwieh <overexposedgame@gmail.com>
2025-05-09 01:27:22 +0100

    Update paranoia-online-instructions.js

--- public/scripts/party-games/paranoia/paranoia-online-instructions.js
+++ public/scripts/party-games/paranoia/paranoia-online-instructions.js
@@ -31,6 +31,7 @@ async function NextQuestion() {
   const currentPartyData = existingData[0];
   const index = currentPartyData.computerIds.indexOf(deviceId);
   console.log("currentPartyData.usersReady[index]: " + currentPartyData.usersReady[index]);
+  waitingForPlayerContainer.classList.remove('active');
   if (currentPartyData.usersReady[index] == false) {
     currentPartyData.usersReady[index] = true;
     nextQuestionContainer.classList.add('active');
